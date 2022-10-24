55a693508f426bdc10cd9f0a437a2ea6bb9f7e0d
zzall <1809642355@qq.com>
2022-10-24 21:36:19 +0800

    feat:init snippets

---
+++ README.md
@@ -0,0 +1,44 @@
+# Snippets For React/Vue/JS/TS
+
+一个Snippets插件集成了市面上安装量较大的Vue 3 Snippets、Simple React Snippets，另新增一些常用js、ts语法的snippets
+
+## Features
+
+下面简单介绍一下可使用的`snippets`有哪些
+
+* 在`React`文件中
+![React Snippets](images/react-ex1.gif)
+![React Snippets](images/react-ex2.gif)
+
+---
+
+* 在`Vue`文件中
+![Vue Snippets](images/vue-ex1.gif)
+![Vue Snippets](images/vue-ex2.gif)
+
+---
+
+* 在`js/ts`文件中
+![js/ts Snippets](images/js-ex1.gif)
+
+---
+
+* 在`json`文件中
+![json snippets](images/json-exq.gif)
+
+
+## Known Issues
+
+暂无
+
+## Release Notes
+
+### 1.0.0
+
+初始化`React Snippets`、`Vue 2 Snippets`、`JS`、`TS`
+
+---
+
+**快来尝试下吧!**
+
+
---
+++ src/extension.ts
@@ -0,0 +1,39 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "snippets-for-react-vue-other" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('snippets-for-react-vue-other.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello VS Code!');
+	});
+	let disposableTest = vscode.commands.registerCommand('snippets-for-react-vue-other.test', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello VS Code!--test');
+	});
+	let disposableTest2 = vscode.commands.registerCommand('snippets-for-react-vue-other.test2', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello VS Code!--test2');
+	});
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableTest);
+	context.subscriptions.push(disposableTest2);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
